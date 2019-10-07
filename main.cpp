cad8dae58e3230e13d1ac3bfc51ff007dd04fb5f
Alexey Gorbachev <Alexey1020@gmail.com>
2019-10-07 19:21:40 +0300

    starting

--- main.cpp
+++ main.cpp
@@ -1,24 +1,44 @@
-#define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
 #include <string>
 #include <iomanip>
 #include <random>
-#include <map>
-#include <unordered_map>
 static std::mt19937 generator{ std::random_device{}() };
 
-double func(double x, double y){
+class Genethic {
+private:
+    double x;
+    double y;
+    double left_lim = -2.2;
+    double right_lim = 2.2;
+public:
+    Genethic();
+    double func(double, double);
+    void mutation(std::unique_ptr<double[]>);
+    void crossover();
+    bool run();
+
+};
+Genethic::Genethic() {
+    std::uniform_real_distribution<> dist{left_lim, right_lim};
+    x = dist(generator);
+    y = dist(generator);
+
+}
+
+void Genethic::mutation(std::unique_ptr<double[]> Cordinates) {
+
+}
+
+double Genethic::func(double x, double y){
     return exp(-pow(x,2))*exp(-pow(y, 2)) / (1+pow(x,2) + pow(y, 2));
 }
 
 int main()
 {
-    std::uniform_real_distribution<> dis{-2.2, 2.2};
-    double x1 = dis(generator), x2 = dis(generator), x3 = dis(generator), x4 = dis(generator);
-    std::pair<double, double> alpha1(x1, x2);
-    std::pair<double, double> alpha2(x3, x4);
-    std::swap(alpha1.second, alpha2.second);
-    std::cout << "";
+    Genethic gen_algorithm;
+    std::unique_ptr<double[]> cordinates(new double[4]);
+    gen_algorithm.mutation(std::move(cordinates));
+
     return 0;
 }
